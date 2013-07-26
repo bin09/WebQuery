3d6a8301cadcd0e33bbac7826192d512b497004d
bin <ylgw0909@gmail.com>
2013-07-26 10:57:19 +0800

    WebQuery

---
+++ DAL/BaseDal.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DBUtility;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    /// <summary>
+    /// 基础数据层
+    /// </summary>
+    public abstract class BaseDal
+    {
+        private readonly string ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        #region 取得列表
+        /// <summary>
+        /// 取得数据列表
+        /// </summary>
+        /// <param name="strSql">Sql语句</param>
+        /// <param name="parameters">参数</param>
+        /// <returns></returns>
+        public DataSet ExecuteDataset(string strSql, params SqlParameter[] parameters)
+        {
+            return SqlHelper.ExecuteDataset(this.ConnectionString, CommandType.Text, strSql, parameters);
+        }
+        /// <summary>
+        /// 取得数据列表
+        /// </summary>
+        /// <param name="strSql">Sql语句</param>
+        /// <returns></returns>
+        public DataSet ExecuteDataset(string strSql)
+        {
+            return SqlHelper.ExecuteDataset(this.ConnectionString, CommandType.Text, strSql.ToString());
+        }
+        /// <summary>
+        /// 执行存储过程取得数据列表
+        /// </summary>
+        /// <param name="spName">存储过程名称</param>
+        /// <returns></returns>
+        public DataSet SP_ExecuteDataset(string spName)
+        {
+            return SqlHelper.ExecuteDataset(this.ConnectionString, CommandType.StoredProcedure, spName);
+        }
+        /// <summary>
+        /// 执行存储过程取得数据列表
+        /// </summary>
+        /// <param name="spName">存储过程名称</param>
+        /// <param name="parameters">参数</param>
+        /// <returns></returns>
+        public DataSet SP_ExecuteDataset(string spName, params SqlParameter[] parameters)
+        {
+            return SqlHelper.ExecuteDataset(this.ConnectionString, CommandType.StoredProcedure, spName, parameters);
+        }
+        #endregion
+
+        #region 执行SQL语句
+        /// <summary>
+        /// 执行SQL语句
+        /// </summary>
+        /// <param name="strSql">Sql语句</param>
+        /// <returns></returns>
+        public int ExecuteNonQuery(string strSql)
+        {
+            return SqlHelper.ExecuteNonQuery(ConnectionString, CommandType.Text, strSql);
+        }
+        /// <summary>
+        /// 执行SQL语句
+        /// </summary>
+        /// <param name="strSql">Sql语句</param>
+        /// <param name="parameters">参数</param>
+        /// <returns></returns>
+        public int ExecuteNonQuery(string strSql, params SqlParameter[] parameters)
+        {
+            return SqlHelper.ExecuteNonQuery(ConnectionString, CommandType.Text, strSql, parameters);
+        }
+        /// <summary>
+        /// 执行存储过程
+        /// </summary>
+        /// <param name="spName">存储过程名称</param>
+        /// <returns>返回影响行数</returns>
+        public int SP_ExecuteNonQuery(string spName)
+        {
+            return SqlHelper.ExecuteNonQuery(ConnectionString, CommandType.StoredProcedure, spName);
+        }
+        /// <summary>
+        /// 执行存储过程
+        /// </summary>
+        /// <param name="spName">存储过程名称</param>
+        /// <param name="parameters">参数</param>
+        /// <returns>返回影响行数</returns>
+        public int SP_ExecuteNonQuery(string spName, params SqlParameter[] parameters)
+        {
+            return SqlHelper.ExecuteNonQuery(ConnectionString, CommandType.StoredProcedure, spName, parameters);
+        }
+        #endregion
+
+        #region 取得单记录
+        /// <summary>
+        /// 取得第一行的第一个字段
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public object ExecuteScalar(string sql)
+        {
+            return SqlHelper.ExecuteScalar(this.ConnectionString, CommandType.Text, sql);
+        }
+        /// <summary>
+        /// 得第一行的第一个字段
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
+        public object ExecuteScalar(string sql, params SqlParameter[] pars)
+        {
+            return SqlHelper.ExecuteScalar(this.ConnectionString, CommandType.Text, sql, pars);
+        }
+        /// <summary>
+        /// 存储过程得第一行的第一个字段
+        /// </summary>
+        /// <param name="spName">存储过程名称</param>
+        /// <returns></returns>
+        public object SP_ExecuteScalar(string spName)
+        {
+            return SqlHelper.ExecuteScalar(this.ConnectionString, CommandType.StoredProcedure, spName);
+        }
+        /// <summary>
+        /// 得第一行的第一个字段
+        /// </summary>
+        /// <param name="sql">存储过程名称</param>
+        /// <param name="pars">参数</param>
+        /// <returns></returns>
+        public object SP_ExecuteScalar(string sql, params SqlParameter[] pars)
+        {
+            return SqlHelper.ExecuteScalar(this.ConnectionString, CommandType.Text, sql, pars);
+        }
+        #endregion
+
+        #region 执行Reader SQL语句
+        /// <summary>
+        /// 执行Reader SQL语句
+        /// </summary>
+        /// <param name="strSql">Sql语句</param>
+        /// <param name="parameters">参数</param>
+        /// <returns></returns>
+        public SqlDataReader ExecuteReader(string strSql, params SqlParameter[] parameters)
+        {
+            return SqlHelper.ExecuteReader(ConnectionString, CommandType.Text, strSql, parameters);
+        }
+        /// <summary>
+        /// 执行存储过程Reader 语句
+        /// </summary>
+        /// <param name="spName">存储过程名称</param>
+        /// <param name="parameters">参数</param>
+        /// <returns></returns>
+        public SqlDataReader SP_ExecuteReader(string spName, params SqlParameter[] parameters)
+        {
+            return SqlHelper.ExecuteReader(ConnectionString, CommandType.StoredProcedure, spName, parameters);
+        }
+        /// <summary>
+        /// 执行Reader SQL语句
+        /// </summary>
+        /// <param name="strSql">Sql语句</param>
+        /// <returns></returns>
+        public SqlDataReader ExecuteReader(string strSql)
+        {
+            return SqlHelper.ExecuteReader(ConnectionString, CommandType.Text, strSql);
+        }
+        /// <summary>
+        /// 执行存储过程Reader 语句
+        /// </summary>
+        /// <param name="spName">存储过程名称</param>
+        /// <returns></returns>
+        public SqlDataReader SP_ExecuteReader(string spName)
+        {
+            return SqlHelper.ExecuteReader(ConnectionString, CommandType.StoredProcedure, spName);
+        }
+        #endregion
+    }
+}
---
+++ Helper/Log4Helper.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+
+namespace Helper
+{
+    /// <summary>
+    /// Enumeration for database action type
+    /// </summary>
+    public enum DatabaseAction
+    {
+        Select,
+        Insert,
+        Update,
+        Delete
+    }
+    public class Log4Helper
+    {
+        //Pre-defined logger name in web.config or app.config
+        private const string LoggerName = "Query";
+        private const int EmptyFunctionKey = -1;
+        private const int AnonymouseUserKey = -1;
+        private static readonly ILog Logger = log4net.LogManager.GetLogger(LoggerName);
+
+
+        /// <summary>
+        /// Add a error log
+        /// </summary>
+        /// <param name="errorMessage"></param>
+        public static void AddError(string errorMessage)
+        {
+            Logger.Error(errorMessage);
+        }
+        /// <summary>
+        /// 添加异常具体信息
+        /// </summary>
+        /// <param name="ex"></param>
+        public static void AddError(Exception ex)
+        {
+            Logger.Error(System.Environment.NewLine + ex.Message + System.Environment.NewLine + ex.ToString());
+        }
+        /// <summary>
+        /// Add a error log with an exception
+        /// </summary>
+        /// <param name="errorMessage"></param>
+        /// <param name="ex"></param>
+        public static void AddError(string errorMessage, Exception ex)
+        {
+            Logger.Error(errorMessage + System.Environment.NewLine + ex.Message + System.Environment.NewLine + ex.ToString());
+        }
+        /// <summary>
+        /// Add a warning log
+        /// </summary>
+        /// <param name="warnMessage"></param>
+        public static void AddWarn(string warnMessage)
+        {
+            Logger.Warn(warnMessage);
+        }
+        /// <summary>
+        /// Add a warning log with an exception
+        /// </summary>
+        /// <param name="warnMessage"></param>
+        /// <param name="ex"></param>
+        public static void AddWarn(string warnMessage, Exception ex)
+        {
+            Logger.Warn(warnMessage + System.Environment.NewLine + ex.Message + System.Environment.NewLine + ex.ToString());
+        }
+        /// <summary>
+        /// Add a info log
+        /// </summary>
+        /// <param name="infoMessage"></param>
+        public static void AddInfo(string infoMessage)
+        {
+            Logger.Info(infoMessage);
+        }
+        /// <summary>
+        /// Add a info log with an exception
+        /// </summary>
+        /// <param name="infoMessage"></param>
+        /// <param name="ex"></param>
+        public static void AddInfo(string infoMessage, Exception ex)
+        {
+            Logger.Info(infoMessage + System.Environment.NewLine + ex.Message + System.Environment.NewLine + ex.ToString());
+        }
+        /// <summary>
+        /// Add a debug log
+        /// </summary>
+        /// <param name="debugMessage"></param>
+        public static void AddDebug(string debugMessage)
+        {
+            Logger.Debug(debugMessage);
+        }
+        /// <summary>
+        /// Add a debug log with an exception
+        /// </summary>
+        /// <param name="debugMessage"></param>
+        /// <param name="ex"></param>
+        public static void AddDebug(string debugMessage, Exception ex)
+        {
+            Logger.Debug(debugMessage + System.Environment.NewLine + ex.Message + System.Environment.NewLine + ex.ToString());
+        }
+        /// <summary>
+        /// Add a database error log 
+        /// </summary>
+        /// <param name="objectName">table or view name</param>
+        /// <param name="dbAction">either retrieve, update, delete</param>
+        /// <param name="ex"></param>
+        public static void AddDatabaseError(string objectName, DatabaseAction dbAction, Exception ex)
+        {
+            string logMessage = string.Empty;
+            switch (dbAction)
+            {
+                case DatabaseAction.Select:
+                    logMessage = "Error occurred when retrieve data from table " + objectName;
+                    break;
+                case DatabaseAction.Insert:
+                    logMessage = "Error occurred when insert data to table " + objectName;
+                    break;
+                case DatabaseAction.Update:
+                    logMessage = "Error occurred when update data to table " + objectName;
+                    break;
+                case DatabaseAction.Delete:
+                    logMessage = "Error occurred when delete data from table " + objectName;
+                    break;
+            }
+            AddError(logMessage, ex);
+        }
+    }
+}
